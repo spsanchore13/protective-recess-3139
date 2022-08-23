f4ef974a68df0188afbc2b9eec93132872202edc
spsanchore13 <shantilalpatliya18@gmail.com>
2022-08-23 17:46:19 +0530

    my cw project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { Text } from "@chakra-ui/react"
+
+function App() {
+  return (
+    <div className="App">
+
+      <Text>hello Shantila patliya</Text>
+    </div>
+  );
+}
+
+export default App;
